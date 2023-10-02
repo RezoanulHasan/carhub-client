3b03691f1b1210356d36e830674006077212ceb2
RezoanulHasan <rezoanulhasan96@gmail.com>
2023-10-02 19:40:43 +0600

    add route setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,36 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+import Error from './Component/Shared/Error/Error.jsx';
+import Home from './Component/Pages/HOME/Home';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element:<Home></Home>,
+      },
+    ],
+  },
+]);
+
+
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
+
+
 )
